721d2939e7728eec35355788e0634eaa68e01bfa
mickeybhenson-commits <mickey.b.henson@gmail.com>
2026-03-16 15:30:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -219,8 +219,8 @@ if run_button:
                 get_position="[lon, lat]",
                 get_fill_color="color",
                 get_radius="radius",
-                radius_min_pixels=4,
-                radius_max_pixels=8,
+                radius_min_pixels=2,
+                radius_max_pixels=4,
                 pickable=True,
             )
 
